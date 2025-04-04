cb8b9d33eeb96113ab6da7fd3529d8d3a5a9f0d6
Samed763 <sdinc763@gmail.com>
2025-04-04 17:30:57 +0300

    fixed

---
+++ philo_one/utils.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadinc <sadinc@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/10 12:40:45 by ncolomer          #+#    #+#             */
+/*   Updated: 2025/04/04 17:22:13 by sadinc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int
+	ft_strlen(char const *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int
+	ft_atoi(char const *str)
+{
+	int	i;
+	int	is_neg;
+	int	res;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' ||
+			str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+		i++;
+	is_neg = (str[i] == '-') ? -1 : 1;
+	if (is_neg == -1 || str[i] == '+')
+		i++;
+	res = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+		res = (res * 10) + (str[i++] - '0');
+	return (res * is_neg);
+}
+
+void
+	ft_putnbr_fd(long long n, int fd)
+{
+	char	str[13];
+	int		length;
+
+	if (n == 0)
+		str[0] = '0';
+	length = 0;
+	while (n != 0)
+	{
+		str[length++] = '0' + (n % 10);
+		n = (n / 10);
+	}
+	if (length > 0)
+		length--;
+	while (length >= 0)
+		write(fd, &str[length--], 1);
+}
+
+long long
+	get_time(void)
+{
+	static struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * (long long)1000) + (tv.tv_usec / 1000));
+}
---
+++ philo_two/philosophers.h
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadinc <sadinc@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/10 12:40:07 by ncolomer          #+#    #+#             */
+/*   Updated: 2025/04/04 17:23:01 by sadinc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <semaphore.h>
+
+# define SEMAPHORE_FORK		"pSemaphoreFork"
+# define SEMAPHORE_WRITE	"pSemaphoreWrite"
+# define SEMAPHORE_DEAD		"pSemaphoreDead"
+# define SEMAPHORE_PHILO	"pSemaphorePhilo"
+# define SEMAPHORE_PHILOEAT	"pSemaphorePhiloEat"
+
+# define TYPE_EAT 	0
+# define TYPE_SLEEP 1
+# define TYPE_FORK 	2
+# define TYPE_THINK	3
+# define TYPE_DIED 	4
+# define TYPE_OVER 	5
+
+struct s_state;
+
+typedef struct		s_philo
+{
+	int				position;
+	int				is_eating;
+	long long		limit;
+	long long		last_eat;
+	int				lfork;
+	int				rfork;
+	struct s_state	*state;
+	int				eat_count;
+	sem_t			*mutex;
+	sem_t			*eat_count_m;
+}					t_philo;
+
+typedef struct		s_state
+{
+	int				amount;
+	long long		time_to_die;
+	long long		time_to_eat;
+	long long		time_to_sleep;
+	int				must_eat_count;
+	int				cur_eat_count;
+
+	long long		start;
+	int				over;
+
+	t_philo			*philos;
+	sem_t			*forks_m;
+	sem_t			*write_m;
+	sem_t			*somebody_dead_m;
+}					t_state;
+
+int					ft_strlen(char const *str);
+
+int					ft_atoi(char const *str);
+
+void				ft_putnbr_fd(long long n, int fd);
+
+int					ft_strcpy(char *dst, const char *src);
+
+long long			get_time(void);
+
+int					clear_state(t_state *state);
+
+int					exit_error(char const *str);
+
+int					init(t_state *state, int argc, char const **argv);
+
+sem_t				*ft_sem_open(char const *name, int value);
+
+char				*make_semaphore_name(char const *base,
+											char *buffer, int position);
+
+int					take_forks(t_philo *philo);
+
+int					clean_forks(t_philo *philo);
+
+int					eat(t_philo *philo);
+
+int					display_message(t_philo *philo, int type);
+
+#endif
